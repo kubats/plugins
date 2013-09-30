0a70693a2ef1cbfb198189f1ba2995318a3dbf57
Kevin Dubato <kmd.subscriptions@gmail.com>
2013-09-30 08:20:07 -0500

    Added 2 plugins, kdubModal, kdubHighlighter

---
+++ kdubHighlighter/kdubHighlighter/js/kdHighlighter.js
@@ -0,0 +1,35 @@
+(function($) {
+
+$.fn.kdHighlighter = function(options) {
+
+	var defaults = {
+		color: 'yellow',
+		method : 'click'
+	};
+	
+	options = $.extend(defaults, options);
+	
+	this.each(function() {
+		var $this = $(this);
+		var currentBg = $this.css('background-color');
+		
+		if (options.method == 'click') {
+			$this.toggle(function() {
+				$this.css('background', options.color);
+			}, function() {
+				$this.css('background', currentBg);
+			});
+		}
+		
+		if (options.method == 'hover') {
+			$this.hover(function() {
+				$this.css('background', options.color);
+			}, function() {
+				$this.css('background', currentBg);
+			});
+		}
+	});
+	
+}
+
+})(jQuery)
---
+++ kdubModal/js/kdubModal.js
@@ -0,0 +1,94 @@
+(function($) {
+
+	$.fn.kdubModal = function(options) {
+	
+		var defaults = {
+			closeButtonClick : false,
+			olcolor : '#000',
+			width : '',
+			inline : true,
+			ContentSetsWidth: true,
+			fixed : false
+		};
+		
+		options = $.extend(defaults, options);
+		
+		var $body = $('body');
+		var $window = $(window);
+		
+		var modal = {
+			overlay: $('<div id="overlay" />'),
+			window : $('<div id="kdubModal" />'),
+		}
+		
+		$body.append(modal.overlay, modal.window);
+		modal.content = modal.window.children();
+		
+		modal.init = function() {
+			this.overlay.bind('click', modal.Hide);
+			this.overlay.css({ 'background' : options.olcolor });
+			$window.bind('resize', modal.setPosition);
+		}
+	
+		modal.setPosition = function() {
+			var modalHeight = this.window.outerHeight();
+			var modalWidth = this.window.outerWidth();
+			var viewportWidth = $(window).width();
+			var viewportHeight = $(window).height();
+			modal.overlay.height(viewportHeight);
+			modal.window.animate({
+				'top' : (viewportHeight - modalHeight) / 2,
+				'left' : (viewportWidth - modalWidth) / 2
+			},200);
+		}
+		
+		modal.Show = function()  {
+			this.overlay.show();
+			this.window.fadeIn(); 
+			this.setPosition();
+			return false;
+		}
+		
+		modal.Hide = function() {
+			modal.window.fadeOut(function() { 
+				modal.overlay.hide();
+			});
+			return false;
+		}
+		
+		
+		if ( options.ContentSetsWidth == true ) {
+			modal.window.css({
+				'width' : modal.content.width(),
+			});	
+		}
+		else {
+			modal.window.css({
+				'width' : options.width,
+				'background' : options.modalBg,
+			});
+		}
+		
+		return this.each(function() {
+			
+			var $this = $(this);
+			
+			$this.click(function() {
+				var target = $this.attr('href');
+				
+				if (options.inline === true) {
+					var targetClone = $(target).clone();
+					targetClone.css('display', 'block');
+					modal.window.html(targetClone);
+					modal.content.show();
+					modal.Show();
+					return false;
+				}
+			});
+			modal.init();
+		});
+		
+	}
+
+
+})(jQuery)
